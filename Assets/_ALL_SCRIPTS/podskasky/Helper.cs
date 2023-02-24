9c41273c14944fbe5d883e38b5cab994d465c5f0
olegfury <kurnyshov.03@mail.ru>
2023-02-25 02:01:26 +0500

    25.02.2023
    Провел тесты подсказок , добавил на некоторое оборудование
    Разобрал кип , чтобы позже сделать его сборку

--- Assets/_ALL_SCRIPTS/podskasky/Helper.cs
+++ Assets/_ALL_SCRIPTS/podskasky/Helper.cs
@@ -5,28 +5,34 @@ using UnityEngine;
 
 public class Helper : MonoBehaviour
 {
-    public GameObject podskaska;
+    public GameObject podskaska_whatis_off;
+
     public Transform Player;
     public Transform Object;
+
     public float show_distanse;
     // Start is called before the first frame update
     void Start()
     {
-
+       // PlayerforTargeting = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+// Update is called once per frame
+void Update()
     {
         var distanse = Vector3.Distance(Player.transform.position, Object.transform.position);
 
         if (distanse >= show_distanse)
         {
-            podskaska.SetActive(false);
+            podskaska_whatis_off.SetActive(false);
         }
         else
         {
-            podskaska.SetActive(true);
+            podskaska_whatis_off.SetActive(true);
         }
+
+
     }
 }
